0158ca7bae2ca9881283619f4a2b1cfa50275d60
CodeGizmo <angelolanuza@gmail.com>
2019-02-24 19:15:47 -0500

    [2019-02-24, 7:11p est] First commit, added templates to c/c++, bootstrap, html/css/js.

---
+++ c_cpp/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int argc, int argv[]){
+	std::cout << "Welcome to CodeGizmo!" << std::endl;
+	return 0;
+}
---
+++ c_cpp/main2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(int argc, int argv[]){
+	cout << "Welcome to CodeGizmo!" << endl;
+	return 0;
+}
